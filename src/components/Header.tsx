873037193dcd8430d9a16e3a1c8cae3004dc372a
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-12-15 23:37:22 +0900

    :adhesive_bandage: chore: セマンティクス違反を修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import type { Social } from "@/types";
 import { motion } from "framer-motion";
-import Link from "next/link";
 import { SocialIcon } from "react-social-icons";
 
 type Props = {
@@ -30,27 +29,25 @@ export const Header = ({ socials }: Props) => {
         ))}
       </motion.div>
 
-      <Link href="#contact">
-        <motion.div
-          initial={{
-            x: 500,
-            opacity: 0,
-            scale: 0.5,
-          }}
-          animate={{
-            x: 0,
-            opacity: 1,
-            scale: 1,
-          }}
-          transition={{
-            duration: 1.5,
-          }}
-          className="flex flex-row items-center text-gray-300 cursor-pointer"
-        >
-          <SocialIcon className="cursor-pointer" network="email" fgColor="gray" bgColor="transparent" />
-          <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get In Touch</p>
-        </motion.div>
-      </Link>
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
+        <SocialIcon className="cursor-pointer" url="#contact" network="email" fgColor="gray" bgColor="transparent" />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get In Touch</p>
+      </motion.div>
     </header>
   );
 };
